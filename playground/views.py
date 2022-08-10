27344f6f963a2014f7483d5fb28b6001f2dac4cc
Asif-GD <asif.coach@gmail.com>
2022-08-10 19:19:09 +0530

    new route - playground/hello/
    - defined a new function say_hello
    - it serves hello.html

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# request -> response
+# request handler
+# a.k.a an action
+
+
+def say_hello(request):
+    # can be used to pull data from a DB
+    # transform data
+    # send email
+    return render(request, "hello.html", {"name": "Asif Iqbal"})
